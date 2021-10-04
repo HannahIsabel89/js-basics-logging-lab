c429acdf1bdd5db75f887349bf076324082dc5bc
Hannah <hannahwoodward@hotmail.com>
2021-10-04 12:31:15 +0800

    didn't understand this lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const driver = 'bob';
+
+console.log('this code was called');
+console.log(spy.calledWithExactly('this code was called'));
+
+console.log(driver);
+console.log(spy.calledWithExactly('bob'));
+
+
